6824ac9287b16faa3a298b91386c36418a45982e
alumno2024 <alumno2024@DESKTOP-H121FO5>
2023-11-22 02:15:24 +0100

    Agregamos la carpeta DataBaseMySQL que contiene el archivo .sql de la base de datos

---
+++ Horarios/DataBaseMySQL/horarios.sql
@@ -0,0 +1,128 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-11-2023 a las 02:12:35
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `horarios`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asignatura`
+--
+
+CREATE TABLE `asignatura` (
+  `id_asignatura` int(11) NOT NULL,
+  `id_horario` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `enlace` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `asignatura`
+--
+
+INSERT INTO `asignatura` (`id_asignatura`, `id_horario`, `nombre`, `enlace`) VALUES
+(1, 5, 'miAsignatura', 'asdasdd.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `horario`
+--
+
+CREATE TABLE `horario` (
+  `id_horario` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `curso` varchar(100) NOT NULL,
+  `descripcion` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `horario`
+--
+
+INSERT INTO `horario` (`id_horario`, `nombre`, `curso`, `descripcion`) VALUES
+(1, 'miHorario', 'DAM', 'Esta es mi descripcion del horario de clase de DAM 23 - 24'),
+(2, 'mihorario', 'daw', NULL),
+(3, 'mihorario', 'daw', NULL),
+(4, 'mihorario2', 'daw', NULL),
+(5, 'mihorario2', '', NULL),
+(6, 'mihorario4', '', NULL),
+(7, '', '', NULL),
+(8, '', '', NULL),
+(9, 'Hola', 'asd', 'dsadsa'),
+(10, '', '', 'hola'),
+(12, 'miMiniHorario100', 'DAM', 'null'),
+(13, 'miMiniHorario100', 'DAM', 'null'),
+(14, 'miMiniHorario100', 'DAM', 'null'),
+(15, 'miMiniHorario100', 'DAM', 'null'),
+(16, 'miMiniHorario100', 'DAM', 'null'),
+(17, 'miMiniHorario100', 'DAM', 'null'),
+(18, 'miMiniHorario100', 'DAM', 'null'),
+(19, 'miMiniHorario100', 'DAM', 'null'),
+(20, 'miMiniHorario100', 'DAM', 'null');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `asignatura`
+--
+ALTER TABLE `asignatura`
+  ADD PRIMARY KEY (`id_asignatura`),
+  ADD KEY `id_horario` (`id_horario`);
+
+--
+-- Indices de la tabla `horario`
+--
+ALTER TABLE `horario`
+  ADD PRIMARY KEY (`id_horario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `asignatura`
+--
+ALTER TABLE `asignatura`
+  MODIFY `id_asignatura` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `horario`
+--
+ALTER TABLE `horario`
+  MODIFY `id_horario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `asignatura`
+--
+ALTER TABLE `asignatura`
+  ADD CONSTRAINT `fk_horario_asignatura` FOREIGN KEY (`id_horario`) REFERENCES `horario` (`id_horario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
